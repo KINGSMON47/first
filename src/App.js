40012564b356f43b2452f78ad87cdf9c6782eef2
kingsmon <mnjsaeed@gmail.com>
2023-12-31 23:20:13 +0330

    bootstrap added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 
-
 function App() {
   return (
-  <div>
-    تست
-  </div>
+    <div>
+     <div className="mb-3">
+<label htmlFor=""></label>
+  <div className="container bg-danger">123</div>
+</div>123134
+<div className="mb-3">
+  <label htmlFor="exampleFormControlTextarea1" className="form-label">Example textarea</label>
+  <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+</div>
+    </div>
   );
 }
 
